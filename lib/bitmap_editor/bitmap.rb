246d8bd7ee6d934919805cbc91f45321048ec487
Liam Bennett <liam@littleowllabs.com>
2017-04-03 22:37:38 +0100

    feat: add clear functionality

--- lib/bitmap_editor/bitmap.rb
+++ lib/bitmap_editor/bitmap.rb
@@ -1,7 +1,21 @@
 module BitmapEditor
+  EMPTY_VALUE = 0
+
   class Bitmap
-    def initialize(width, height)
-      @matrix = Array.new(height) { Array.new(width, 0) }
+    def initialize(width, height, initial_value = EMPTY_VALUE)
+      @width = width
+      @height = height
+      @matrix = Array.new(height) { Array.new(width, initial_value) }
+    end
+
+    def clear
+      fill(EMPTY_VALUE)
+    end
+
+    def fill(value)
+      @matrix.each do |row|
+        row.fill(value)
+      end
     end
 
     def to_s
--- spec/bitmap_editor/bitmap_spec.rb
+++ spec/bitmap_editor/bitmap_spec.rb
@@ -1,8 +1,31 @@
 RSpec.describe BitmapEditor::Bitmap do
   describe 'initialise' do
-    let (:bitmap) { described_class.new(2,3) }
     subject { bitmap.to_s }
 
-    it { is_expected.to eq( "00\n00\n00" )}
+    context 'with the default empty value' do
+      let(:bitmap) { described_class.new(2, 3) }
+      it { is_expected.to eq("00\n00\n00") }
+    end
+
+    context 'with a specified fill value' do
+      let(:bitmap) { described_class.new(2, 3, 'A') }
+      it { is_expected.to eq("AA\nAA\nAA") }
+    end
+  end
+
+  describe '#clear' do
+    let(:bitmap) { described_class.new(2, 3, 'A') }
+    before(:each) { bitmap.clear }
+    subject { bitmap.to_s }
+
+    it { is_expected.to eq("00\n00\n00") }
+  end
+
+  describe '#fill' do
+    let(:bitmap) { described_class.new(2, 3) }
+    before(:each) { bitmap.fill('A') }
+    subject { bitmap.to_s }
+
+    it { is_expected.to eq("AA\nAA\nAA") }
   end
 end
