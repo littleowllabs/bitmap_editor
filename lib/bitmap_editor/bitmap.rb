db162d25a4c33adc049459c5b5d52e51bc6f61ce
Liam Bennett <liam@littleowllabs.com>
2017-04-03 23:43:21 +0100

    feat: add set method to bitmap

--- lib/bitmap_editor/bitmap.rb
+++ lib/bitmap_editor/bitmap.rb
@@ -8,6 +8,14 @@ module BitmapEditor
       @matrix = Array.new(height) { Array.new(width, initial_value) }
     end
 
+    def set(x_min, x_max, y_min, y_max, val)
+      validate_range(x_min, x_max, y_min, y_max)
+      # modify indices to work with 1-base-indexing
+      ((y_min-1)..(y_max-1)).each do |row|
+        @matrix[row].fill(val, x_min-1, x_max-x_min+1)
+      end
+    end
+
     def clear
       fill(EMPTY_VALUE)
     end
@@ -22,6 +30,18 @@ module BitmapEditor
       @matrix.map do |row|
         row.join('')
       end.join("\n")
+    end
+
+    private
+
+    def validate_range(x_min, x_max, y_min, y_max)
+      errors = []
+      errors << "minimum x (#{x_min}) cannot be less than 1" if (x_min < 1)
+      errors << "maximum x (#{x_max}) cannot be greater than #{@width}" if (x_max > @width)
+      errors << "minimum y (#{y_min}) cannot be less than 1" if (y_min < 1)
+      errors << "maximum y (#{y_max}) cannot be greater than #{@height}" if (y_max > @height)
+
+      raise errors.join('') if !errors.empty?
     end
   end
 end
--- spec/bitmap_editor/bitmap_spec.rb
+++ spec/bitmap_editor/bitmap_spec.rb
@@ -1,4 +1,56 @@
 RSpec.describe BitmapEditor::Bitmap do
+  shared_examples_for 'a validated method' do |method, args = []|
+    let(:bitmap) { described_class.new(2, 3) }
+    let(:x_min) { 1 }
+    let(:x_max) { 2 }
+    let(:y_min) { 1 }
+    let(:y_max) { 3 }
+
+    subject { bitmap.send(method, x_min, x_max, y_min, y_max, *args) }
+
+    context 'when all values are valid' do
+      it 'does not raise an error' do
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'when min_x is less than 1' do
+      let(:x_min) { 0 }
+      it 'raises an error' do
+        expect { subject }.to raise_error(
+          RuntimeError, "minimum x (#{x_min}) cannot be less than 1"
+        )
+      end
+    end
+
+    context 'when max_x is greater than the width' do
+      let(:x_max) { 3 }
+      it 'raises an error' do
+        expect { subject }.to raise_error(
+          RuntimeError, "maximum x (#{x_max}) cannot be greater than 2"
+        )
+      end
+    end
+
+    context 'when min_y is less than 1' do
+      let(:y_min) { 0 }
+      it 'raises an error' do
+        expect { subject }.to raise_error(
+          RuntimeError, "minimum y (#{y_min}) cannot be less than 1"
+        )
+      end
+    end
+
+    context 'when max_y is greater than the height' do
+      let(:y_max) { 4 }
+      it 'raises an error' do
+        expect { subject }.to raise_error(
+          RuntimeError, "maximum y (#{y_max}) cannot be greater than 3"
+        )
+      end
+    end
+  end
+
   describe 'initialise' do
     subject { bitmap.to_s }
 
@@ -27,5 +79,17 @@ RSpec.describe BitmapEditor::Bitmap do
     subject { bitmap.to_s }
 
     it { is_expected.to eq("AA\nAA\nAA") }
+  end
+
+  describe '#set' do
+    it_behaves_like 'a validated method', :set, 'A'
+
+    describe 'with all values in range' do
+      let(:bitmap) { described_class.new(4, 4) }
+      before(:each) { bitmap.set(2, 3, 2, 3, 'A') }
+      subject { bitmap.to_s }
+
+      it { is_expected.to eq("0000\n0AA0\n0AA0\n0000") }
+    end
   end
 end
